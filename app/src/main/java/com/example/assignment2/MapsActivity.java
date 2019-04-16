164624dcd5e211346c8330c65ca2be294c7246a3
Sophie <[sophiegrimme@gmx.de]>
2019-04-16 17:26:53 +0200

    added current location and marker

--- app/src/main/java/com/example/assignment2/MapsActivity.java
+++ app/src/main/java/com/example/assignment2/MapsActivity.java
@@ -2,45 +2,104 @@ package com.example.assignment2;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.Task;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+{
 
+    private static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap;
+    private CameraPosition mCameraPosition;
+
+
+    // geographical Location where the device is currently located
+    private Location mLastKnownLocation;
+
+    // Keys for storing activity state
+    private static final String KEY_CAMERA_POSITION = "camera_position";
+    private static final String KEY_LOCATION = "location";
+
+    private final LatLng mDefaultLocation;
+    private static final int DEFAULT_ZOOM = 15;
+
+    //warum???
+    public MapsActivity(LatLng mDefaultLocation) {
+        this.mDefaultLocation = mDefaultLocation;
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        //source: https://developers.google.com/maps/documentation/android-sdk/current-place-tutorial
+        if (savedInstanceState != null) {
+            mLastKnownLocation = savedInstanceState.getParcelable(KEY_LOCATION);
+            mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
+        }
+
+        // Get content view
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+
+        // Build the map
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    //source: https://developers.google.com/maps/documentation/android-sdk/current-place-tutorial
+    private void getDeviceLocation (@NonNull Task<Location> task)
+    {
+        if (task.isSuccessful())
+        {
+            // Set the map's camera position to the current location of the device.
+            mLastKnownLocation = task.getResult();
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(mLastKnownLocation.getLatitude(),
+                            mLastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+
+            // Set a marker at current position
+            mMap.addMarker(new MarkerOptions().position(new LatLng(mLastKnownLocation.getLatitude(),
+                    mLastKnownLocation.getLongitude())).title("Marker in your current position"));
+        }
+        else
+        {
+            Log.d(TAG, "Current location is null. Using defaults.");
+            Log.e(TAG, "Exception: %s", task.getException());
+            mMap.moveCamera(CameraUpdateFactory
+                    .newLatLngZoom(mDefaultLocation, DEFAULT_ZOOM));
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
+
+
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)
+        {
         mMap = googleMap;
 
+
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(mLastKnownLocation).title("Marker in your current position"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(mCameraPosition));
+        }
 }
+
