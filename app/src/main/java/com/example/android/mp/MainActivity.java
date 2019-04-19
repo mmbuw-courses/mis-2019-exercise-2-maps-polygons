ee9eda77c40f5364b0e6e976b96f99b65097708b
Olli Singler <singler@cl.uni-heidelberg.de>
2019-04-19 22:26:48 +0200

    Finished task 2a

--- app/src/main/java/com/example/android/mp/MainActivity.java
+++ app/src/main/java/com/example/android/mp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.mp;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -9,16 +14,23 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
     private static final String TAG = "MainActivity";
     private static final int ERROR_DIALOG_REQUEST = 9001;
@@ -28,11 +40,68 @@ public class MainActivity extends AppCompatActivity {
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
     private GoogleMap mMap;
 
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+
+    public static final String MyPREFERENCES = "MapLocations" ;
+    SharedPreferences sharedpreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //https://stackoverflow.com/a/27531283/6118088
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MainActivity.this);
+
+        sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+
+        //https://javapapers.com/android/get-current-location-in-android/
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        getLocationPermission();
+        if(mLocationPermissionGranted) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 100, this);
+        }
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        Toast.makeText(getApplicationContext(), "Map ready", Toast.LENGTH_SHORT).show();
+        mMap = googleMap;
+
+        //https://www.programcreek.com/java-api-examples/index.php?api=com.google.android.gms.maps.GoogleMap.OnMapLongClickListener
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng position) {
+
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                EditText nameInput = findViewById(R.id.TextInput);
+                String nameText = nameInput.getText().toString();
+
+                mMap.addMarker(new MarkerOptions().position(position).title(nameText));
+
+                //https://www.tutorialspoint.com/android/android_shared_preferences.htm
+                editor.putString("Name", nameText);
+                editor.putString("Location", position.toString());
+                editor.apply();
+            }
+        });
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        //https://stackoverflow.com/q/37682414/6118088
+        LatLng position = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(position).title("My current location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+    }
+    public void onProviderDisabled(String provider) {}
+    public void onProviderEnabled(String provider) {}
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
 
     public boolean isServiesOK() {
         Log.d(TAG, "isServicesOK: checking google services version");
@@ -51,8 +120,6 @@ public class MainActivity extends AppCompatActivity {
         String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION};
 
-        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
-                FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
             if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                     COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
                 mLocationPermissionGranted = true;
@@ -61,19 +128,6 @@ public class MainActivity extends AppCompatActivity {
                         permissions,
                         LOCATION_PERMISSION_REQUEST_CODE);
             }
-        }
-
-    }
-
-    private void initMap(){
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-
-        mapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                mMap = googleMap;
-            }
-        });
     }
 
     @Override
@@ -94,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+
 }
 
 
