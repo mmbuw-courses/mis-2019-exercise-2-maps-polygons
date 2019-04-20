df4a4421fa3c2cecc322ac9693cf0ba93edac146
Olli Singler <singler@cl.uni-heidelberg.de>
2019-04-20 16:25:59 +0200

    Finished task 2b

--- app/src/main/java/com/example/android/mp/MainActivity.java
+++ app/src/main/java/com/example/android/mp/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.android.mp;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -31,6 +33,12 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.maps.android.SphericalUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener {
 
@@ -46,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public static final String MyPREFERENCES = "MapLocations";
     SharedPreferences sharedpreferences;
     private boolean polygonStarted = false;
+    private ArrayList<LatLng> polyList = new ArrayList<LatLng>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         getLocationPermission();
         if(mLocationPermissionGranted) {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 100, this);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 250, this);
         }
     }
 
@@ -88,6 +97,10 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 editor.putString("Name", nameText);
                 editor.putString("Location", position.toString());
                 editor.apply();
+
+                if(polygonStarted){
+                    polyList.add(position);
+                }
             }
         });
     }
@@ -98,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         //https://stackoverflow.com/q/37682414/6118088
         LatLng position = new LatLng(location.getLatitude(),location.getLongitude());
         mMap.addMarker(new MarkerOptions().position(position).title("My current location"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(position));
     }
     public void onProviderDisabled(String provider) {}
     public void onProviderEnabled(String provider) {}
@@ -109,24 +122,44 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Button polygonButton = findViewById(R.id.button);
 
         polygonStarted = !polygonStarted;
-        if (polygonStarted)
-            polygonButton.setText(R.string.stop_polygon);
-        else
-            polygonButton.setText(R.string.start_polygon);
-    }
-
-
-    public boolean isServiesOK() {
-        Log.d(TAG, "isServicesOK: checking google services version");
-
-        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
-
-        if (available == ConnectionResult.SUCCESS) {
-            Log.d(TAG, "isServicesOK: Google Play Services is working");
-            return true;
-            //checks that correct google services version contained within users phone
+        if (polygonStarted) {
+            polygonButton.setText(R.string.end_polygon);
         }
-        return false;
+        else {
+            polygonButton.setText(R.string.start_polygon);
+
+            if(polyList.size() >= 3){
+                double area = 0;
+
+                //https://developers.google.com/maps/documentation/android-sdk/shapes
+                PolygonOptions rectOptions = new PolygonOptions().fillColor(Color.argb(50, 0, 0, 0));
+
+
+                double latSum = .0;
+                double longSum = .0;
+
+                //https://stackoverflow.com/a/18444984/6118088
+                for(LatLng point : polyList){
+                    rectOptions.add(point);
+                    latSum += point.latitude;
+                    longSum += point.longitude;
+                }
+
+                latSum = latSum / polyList.size();
+                longSum = longSum / polyList.size();
+
+                mMap.addPolygon(rectOptions);
+
+                //https://developers.google.com/maps/documentation/android-sdk/utility/
+                area = SphericalUtil.computeArea(polyList);
+
+                LatLng centroidPosition = new LatLng(latSum, longSum);
+                String areaText = area < 1000000 ?  Math.floor(area * 100) / 100 + " m²" : Math.floor((area/1000000) * 100) / 100 + " km²";
+                mMap.addMarker(new MarkerOptions().position(centroidPosition).title(areaText));
+            }
+
+            polyList.clear();
+        }
     }
 
     private void getLocationPermission(){
