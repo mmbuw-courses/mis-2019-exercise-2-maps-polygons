1c6bdf139b9dfc246cab9fec0cc9b4069f384281
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-04-21 12:07:28 +0200

    Fixed bugs with text being used Added longclick listener

--- app/src/main/java/com/example/exer02/MapsActivity.java
+++ app/src/main/java/com/example/exer02/MapsActivity.java
@@ -12,6 +12,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -23,19 +24,30 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    LocationManager locationManager;
-    LocationListener locationListener;
-    EditText userText;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private EditText userText;
+
+    public void centerMap(Location locaiton, String title) {
+        LatLng userLocation = new LatLng(locaiton.getLatitude(), locaiton.getLongitude());
+
+        if (title != "Your Location") {
+            mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+        }
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 10));
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(requestCode == 1){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0, locationListener);}
-
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+                }
+                Location lastKnown = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centerMap(lastKnown, "Your Location");
             }
         }
     }
@@ -43,42 +55,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            //ask for permission
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }else{
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, locationListener);
-        }
-
-        userText = findViewById(R.id.userInput);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        userText = findViewById(R.id.userInput);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        //mMap.setOnMapLongClickListener(this);
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                if(!userText.getText().toString().equals("")){
+                    String location = userText.getText().toString();
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                }
+                else Toast.makeText(MapsActivity.this, "Please enter details at top", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+        Location firstLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        LatLng firstLtLn = new LatLng(firstLocation.getLatitude(),firstLocation.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(firstLtLn, 10));
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                final LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
-
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
-
-                mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
-                    @Override
-                    public void onMapLongClick(LatLng latLng) {
-                        mMap.addMarker(new MarkerOptions().position(userLocation).title(userText.toString()));
-                        mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
-                    }
-                });
+                centerMap(location, "Your Location");
             }
 
             @Override
@@ -96,5 +106,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         };
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            //asks for permission
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }else{
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,0, locationListener);
+            Location lastKnown = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            centerMap(lastKnown, "Your Location");
+        }
     }
 }
