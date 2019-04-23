eb5ac435eca619c63d8163181ac5c162a9e5d37a
Sophie <[sophiegrimme@gmx.de]>
2019-04-23 22:50:22 +0200

    build button, text changes

--- app/src/main/java/com/example/assignment2/MapsActivity.java
+++ app/src/main/java/com/example/assignment2/MapsActivity.java
@@ -11,6 +11,8 @@ import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.android.gms.maps.GoogleMap;
@@ -33,6 +35,9 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     protected boolean gps_enabled, network_enabled;
     private boolean mLocationPermissionGranted = false;
     private SharedPreferences sharedPref;
+    private Button polygonButton;
+    private boolean draw_polygon = false;
+    private int polygon_markers = 0;
 
 
 
@@ -88,16 +93,35 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         // Source: https://javapapers.com/android/get-current-location-in-android/
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-        if (mLocationPermissionGranted == true)
-        {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 1000, this);
+        if (mLocationPermissionGranted == true) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 500, this);
         }
 
         // Source: https://developer.android.com/training/data-storage/shared-preferences#java
         sharedPref = getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
 
 
+        // Source: https://developer.android.com/reference/android/widget/Button
+        polygonButton = findViewById(R.id.button);
+        polygonButton.setText("Start Polygon");
+        polygonButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // what happens when you click the button
+                if (!draw_polygon) {
+                    draw_polygon = true;
+                    polygonButton.setText("End Polygon");
+                }
+                else {
+                    draw_polygon = false;
+                    polygonButton.setText("Start Polygon");
+                }
+            }
+        });
+
+
     }
+
 
 
     // Source: https://javapapers.com/android/get-current-location-in-android/
@@ -133,6 +157,7 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 String newMarkerText = newMarker.getText().toString();
                 mMap.addMarker(new MarkerOptions().position(latLng).title(newMarkerText));
 
+                // Source: https://developer.android.com/training/data-storage/shared-preferences#java
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putString("Marker Name", newMarkerText);
                 editor.putString("Position", latLng.toString());
