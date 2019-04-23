be22849d3b651c6653840ffe3c744035d7a07e35
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-04-23 11:13:46 +0200

    Calculates area based on polygon puts a pin in centroid Displays value in meter square

--- app/src/main/java/com/example/exer02/MapsActivity.java
+++ app/src/main/java/com/example/exer02/MapsActivity.java
@@ -4,18 +4,20 @@ import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.text.Html;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,10 +25,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.maps.android.SphericalUtil;
 import java.util.ArrayList;
-import java.util.Map;
-import java.util.Set;
+import java.util.List;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -34,10 +37,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     LocationManager locationManager;
     LocationListener locationListener;
     EditText userText;
-    Boolean firstPin = true;
-    Boolean test = false;
-    ArrayList<Marker> markers = new ArrayList<Marker>();
+    Boolean polygonActive = false;
+    ArrayList<Marker> markers = new ArrayList<>();
     SharedPreferences markerPreferences;
+    Button polyButton;
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -81,6 +84,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.clear();
     }
 
+    public void calculateArea(){
+        //https://stackoverflow.com/questions/28838287/calculate-the-area-of-a-polygon-drawn-on-google-maps-in-an-android-application
+        //http://googlemaps.github.io/android-maps-utils/javadoc/
+        //Area calculation
+        List<LatLng> latLngs = new ArrayList<>();
+        for(int i=0;i< markerPreferences.getInt("listSize",0);i++){
+           latLngs.add(new LatLng(markers.get(i).getPosition().latitude,markers.get(i).getPosition().longitude));
+        }
+        //https://stackoverflow.com/questions/1583940/how-do-i-get-the-first-n-characters-of-a-string-without-checking-the-size-or-goi
+        double area = SphericalUtil.computeArea(latLngs);
+        String formatArea = Double.toString(area);
+        formatArea = formatArea.substring(0, Math.min(formatArea.length(), 4));
+
+
+        //Calculating center
+        //https://stackoverflow.com/questions/18440823/how-do-i-calculate-the-center-of-a-polygon-in-google-maps-android-api-v2
+
+        double centLat = 0.0;
+        double centLong = 0.0;
+
+        for (int i = 0; i < markerPreferences.getInt("listSize",0); i++) {
+            centLat += markers.get(i).getPosition().latitude;
+            centLong += markers.get(i).getPosition().longitude;
+        }
+
+        int totalPoints = markerPreferences.getInt("listSize",0);
+        centLat = centLat / totalPoints;
+        centLong = centLong / totalPoints;
+
+        LatLng latLng = new LatLng(centLat,centLong);
+
+        //https://stackoverflow.com/questions/22255231/using-superscript-in-android
+        mMap.addMarker(new MarkerOptions().position(latLng).title("Roughly: "+formatArea+" "+Html.fromHtml("m<sup>2</sup>")));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -90,6 +128,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         markerPreferences = this.getSharedPreferences("com.example.exer02", Context.MODE_PRIVATE);
         userText = findViewById(R.id.userInput);
+        polyButton = findViewById(R.id.polygon);
     }
 
     @Override
@@ -106,17 +145,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     //deleteData();
                     saveData();
                     loadData();
-                    /*
-                    if(markers.size() == 5){ mMap.clear();
-                    Toast.makeText(MapsActivity.this, "Restarting", Toast.LENGTH_SHORT).show();
-                    test = true;}
-                    if(test){
-                    for (int i=0; i<3;i++){
-                        mMap.addMarker(new MarkerOptions().position((markers.get(i)).getPosition()).title((markers.get(i)).getTitle()));
-                    }}
-                    System.out.println(markers);*/
                 }
                 else Toast.makeText(MapsActivity.this, "Please enter details at top", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        polyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!polygonActive){
+                    //deleteData();
+                    loadData();
+                    PolygonOptions polygonOptions = new PolygonOptions();
+                    int size = markerPreferences.getInt("listSize",0);
+                    for (int i=0;i<size;i++){
+                        polygonOptions.add(new LatLng(markers.get(i).getPosition().latitude,markers.get(i).getPosition().longitude));
+                    }
+                    //https://stackoverflow.com/questions/14326482/android-maps-v2-polygon-transparency
+                    polygonOptions.strokeColor(Color.argb(125,255,0,0)).fillColor(Color.argb(125,0,255,0));
+                    mMap.addPolygon(polygonOptions);
+                    calculateArea();
+                    polyButton.setText("Stop Polygon");
+                    polygonActive = true;
+                }else{
+                    loadData();
+                    polygonActive = false;
+                    polyButton.setText("Start Polygon");
+                }
             }
         });
 
